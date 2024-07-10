cbdb7534d845c094e2b3c473441936d63a98a4ba
Nikson3000 <nskrasnopeorov1997@yandex.ru>
2024-07-10 06:07:03 +0300

    run.sh changes

--- run.sh
+++ run.sh
@@ -21,7 +21,10 @@ if [[ $* == *"--with-obstacles"* ]]
 		-e './run_in_terminal_window.sh ArduCopter sitl_obstacles/bin/arducopter -S --model + --speedup 1 --slave 0 --serial5=tcp:5765:wait --serial6=tcp:5766:wait --serial7=tcp:5767:wait --defaults copter.parm --sim-address=127.0.0.1 --home=53.1019446,107.3774394,846.22,0 -I0' & disown
 	else
 		xterm -geometry 100x30 -bg '#300924' -fg white \
-		-e './run_in_terminal_window.sh ArduCopter sitl/bin/arducopter -S --model + --speedup 1 --slave 0 --serial5=tcp:5765:wait --serial6=tcp:5766:wait --serial7=tcp:5767:wait --defaults copter.parm --sim-address=127.0.0.1 --home=53.1019446,107.3774394,846.22,0 -I0' & disown
+		-e './run_in_terminal_window.sh ArduCopter sitl/bin/arducopter -S --model + --speedup 1 --slave 0 --serial5=tcp:5765:wait --serial6=tcp:5766:wait --serial7=tcp:5767:wait --defaults copter.parm --sim-address=127.0.0.1 --home=46.6143886,142.8119468,69.20,0 -I0' & disown
 fi
 cd ../mavproxy
 python3 MAVProxy/mavproxy.py --out 127.0.0.1:14550 --out 127.0.0.1:14551 --master tcp:127.0.0.1:5760 --sitl 127.0.0.1:5501
+
+# 46.6143886,142.8119468,69.20
+# 53.1019446,107.3774394,846.22
